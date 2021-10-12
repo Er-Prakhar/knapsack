b1fec9ee4ca210ab3d90b9e82dfab72597df3bc5
Er-Prakhar <prakhar369p@gmail.com>
2021-10-12 11:17:37 +0200

    brute_force

--- R/knapsack_brute_force.R
+++ R/knapsack_brute_force.R
@@ -1,4 +1,4 @@
-knapsack_brute_force <- function(x, W, parallel = FALSE) {
+brute_force_knapsack <- function(x, W, parallel = FALSE) {
 
   stopifnot("x must be a data frame with variables v and w" = is.data.frame(x) & all(names(x) %in% c("v", "w")))
 
@@ -6,5 +6,36 @@ knapsack_brute_force <- function(x, W, parallel = FALSE) {
 
   stopifnot("parallel must be a logical value" = is.null(dim(parallel)) & is.logical(parallel))
 
+  x <- x[x$w <= W,]
+
+  n <- nrow(x)
+
+  comb <- matrix(nrow = 2^n-1, ncol = n)
+
+  for(i in 1:2^n-1) {
+    comb[i,] <- as.integer(head(intToBits(i), n))
+  }
+
+  weight <- 0
+
+  for(i in 1:2^n-1) {
+    weight[i] <- sum(x$w[as.logical(comb[i,])])
+  }
+
+  comb <- comb[weight <= W,]
+
+  value <- 0
+
+  for(i in 1:nrow(comb)) {
+    value[i] <- sum(x$v[as.logical(comb[i,])])
+  }
+
+  i <- which.max(value)
+
+  value <- round(value[i])
+
+  elements <- as.integer(rownames(x[as.logical(comb[i,]),]))
+
+  return(list(value = value, elements = elements))
 
 }
